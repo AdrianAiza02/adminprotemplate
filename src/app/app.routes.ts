86186fe0ce90ccee0cfcf131f24138a3808b05fa
AdrianAiza02 <adrianlastword@gmail.com>
2020-07-21 15:23:16 -0400

    modificacion template listo configurado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
-    { 
+    /*{ 
         
         path: '',
         component: PagesComponent,
@@ -18,7 +15,7 @@ const appRoutes: Routes = [
             { path: 'graficas1', component: Graficas1Component },
             { path: '', pathMatch:'full', redirectTo: '/dashboard' }
         ]
-    },
+    },*/
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
